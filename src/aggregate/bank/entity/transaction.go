94d36e62eb7f7150ab1a7b5f1399174e8422da34
Kpaya <matteus.santos03@gmail.com>
2023-08-02 20:16:52 -0300

    feat: fill transaction entity

--- src/aggregate/bank/entity/transaction.go
+++ src/aggregate/bank/entity/transaction.go
@@ -32,8 +32,11 @@ type Withdrawal struct {
 	Amount float64 `json:"amount"`
 }
 
-func (w *Withdrawal) GetAmount() float64 {
-	return w.Amount
+func NewWithdrawal(transaction Transaction, amount float64) *Withdrawal {
+	return &Withdrawal{
+		Transaction: transaction,
+		Amount:      amount,
+	}
 }
 
 type Deposit struct {
@@ -41,6 +44,63 @@ type Deposit struct {
 	Amount float64 `json:"amount"`
 }
 
-func (d *Deposit) GetAmount() float64 {
-	return d.Amount
+func NewDeposit(transaction Transaction, amount float64) *Deposit {
+	return &Deposit{
+		Transaction: transaction,
+		Amount:      amount,
+	}
+}
+
+type CheckDeposit struct {
+	Deposit
+	CheckNumber string `json:"check_number"`
+	BankCode    string `json:"bank_code"`
+}
+
+func NewCheckDeposit(checkNumber, bankCode string, deposit Deposit) *CheckDeposit {
+	return &CheckDeposit{
+		Deposit:     deposit,
+		CheckNumber: checkNumber,
+		BankCode:    bankCode,
+	}
+}
+
+type CashDeposit struct {
+	Deposit
+	CashDepositLimit float64 `json:"cash_deposit_limit"`
+}
+
+func NewCashDeposit(cashDepositLimit float64, deposit Deposit) *CashDeposit {
+	return &CashDeposit{
+		Deposit:          deposit,
+		CashDepositLimit: cashDepositLimit,
+	}
+}
+
+type BalanceInquiry struct {
+	Transaction
+	AccountId string `json:"account_id"`
+}
+
+func NewBalanceInquiry(accountId string, transaction Transaction) *BalanceInquiry {
+	return &BalanceInquiry{
+		Transaction: transaction,
+		AccountId:   accountId,
+	}
+}
+
+type Transfer struct {
+	Transaction
+	FromAccountNumber string  `json:"from_account_number"`
+	ToAccountNumber   string  `json:"to_account_number"`
+	Amount            float64 `json:"amount"`
+}
+
+func NewTransfer(fromAccountNumber, toAccountNumber string, amount float64, transaction Transaction) *Transfer {
+	return &Transfer{
+		Transaction:       transaction,
+		FromAccountNumber: fromAccountNumber,
+		ToAccountNumber:   toAccountNumber,
+		Amount:            amount,
+	}
 }
