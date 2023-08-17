4dde50dad5c7cf75420cbe78f447d876755e50aa
Kpaya <matteus.santos03@gmail.com>
2023-08-16 21:52:46 -0300

    feat: applying interface in account repository

--- src/aggregate/customer/repository/Account.go
+++ src/aggregate/customer/repository/Account.go
@@ -1,24 +1,27 @@
 package repository
 
 import (
+	"github.com/google/uuid"
 	entity "github.com/kpaya/atm-banking/src/aggregate/customer/entity"
 	"gorm.io/gorm"
 )
-
-type AccountRepositoryInterface interface {
-	Get() (*entity.Account, error)
-}
 
 type AccountRepository struct {
 	Db *gorm.DB
 }
 
-func NewAccountRepository(db *gorm.DB) *AccountRepository {
+func NewAccountRepository(db *gorm.DB) IAccountRepository {
 	return &AccountRepository{
 		Db: db,
 	}
 }
 
 func (a *AccountRepository) Get() (*entity.Account, error) {
-	return &entity.Account{}, nil
+
+	return &entity.Account{
+		ID:               uuid.NewString(),
+		AccountNumber:    "####",
+		TotalBalance:     1000,
+		AvaliableBalance: 1000,
+	}, nil
 }
